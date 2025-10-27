43490f715badc399c215d7c06f51e431bdcab73b
Manting Mu <91802193+Manting-Mu@users.noreply.github.com>
2025-10-27 00:21:10 +0000

    Clean up conformer-postprocessing.py
    Removed unused imports and commented-out code sections to clean up the script.

--- small amount of conformer-postprocessing.py
+++ small amount of conformer-postprocessing.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[58]:
 
 
 import sys
@@ -28,7 +27,6 @@ from ase.io import read
 from dscribe.descriptors import SOAP
 
 
-# In[3]:
 
 
 from kneed import KneeLocator
@@ -47,7 +45,6 @@ import sklearn.cluster as cluster
 from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 
 
-# In[4]:
 
 
 # Load the SDF file
@@ -96,8 +93,6 @@ num_filtered_conformers = len(filtered_conformers)
 print(f"Number of conformers within {energy_threshold} kcal/mol: {num_filtered_conformers}")
 
 
-# In[9]:
-
 
 # Now we generate separate SDF files and record the energies
 sdf_files_and_energies = []
@@ -108,9 +103,6 @@ for idx, (i, conformer) in enumerate(filtered_conformers):
             outfile.write(line)
         outfile.write("$$$$\n")
     sdf_files_and_energies.append((output_filename, filtered_energies[idx]))
-
-
-# In[65]:
 
 
 ############# SOAP features ###############
@@ -138,72 +130,6 @@ print(len(soap_features[0]))
 print(range(num_conformers))
 
 
-# import subprocess
-# import os
-# import numpy as np
-# 
-# def calculate_rmsd_matrix(filtered_conformer_list, temp_dir):
-#     """
-#     Calculate the RMSD matrix between conformers in SDF format.
-#     1. Converts SDF to XYZ using Open Babel.
-#     2. Uses `calculate_rmsd` command-line tool to calculate RMSD.
-#     """
-#     num_conformers = len(filtered_conformer_list)
-#     rmsd_matrix = np.zeros((num_conformers, num_conformers))  # Initialize RMSD matrix
-#     
-#     # Write conformer i to an SDF file
-#     #for i in range(num_conformers):
-#     #    sdf1_filename = os.path.join(temp_dir, f"conformer_{i+1}.sdf")
-#     #    xyz1_filename = os.path.join(temp_dir, f"conformer_{i+1}.xyz")
-# 
-#         # Write conformer i to an SDF file
-#     #    with open(sdf1_filename, 'w') as f:
-#     #        f.writelines(filtered_conformer_list[i])
-# 
-#         # Convert SDF to XYZ using Open Babel
-#     #    subprocess.run(['obabel', sdf1_filename, '-O', xyz1_filename], check=True)
-# 
-#     # compute rmsd for conf i and j
-#     for i in range(num_conformers):
-#         sdf1_filename = os.path.join(temp_dir, f"conformer_{i+1}.sdf")
-#         xyz1_filename = os.path.join(temp_dir, f"conformer_{i+1}.xyz")
-#         print(str(i)+"...")
-#         for j in range(i + 1, num_conformers):
-#             sdf2_filename = os.path.join(temp_dir, f"conformer_{i+1}.sdf")
-#             xyz2_filename = os.path.join(temp_dir, f"conformer_{i+1}.xyz")
-#             # Calculate RMSD using the command-line tool
-#             proc = subprocess.check_output(['calculate_rmsd', '--no-hydrogen', '--reorder', xyz1_filename, xyz2_filename],stderr=subprocess.STDOUT)
-#             print(str(j)+",")
-#             rmsd_value = float(proc)
-#             # Store RMSD in the matrix (symmetric)
-#             rmsd_matrix[i, j] = rmsd_value
-#             rmsd_matrix[j, i] = rmsd_value  # Symmetric matrix
-# 
-#     return rmsd_matrix
-# 
-
-# # Create a temporary directory for saving intermediate SDF and XYZ files
-# temp_dir = "temp_conformers/"
-# os.makedirs(temp_dir, exist_ok=True)
-# 
-# # Calculate the RMSD matrix
-# rmsd_matrix = calculate_rmsd_matrix(filtered_conformer_list, temp_dir)
-# 
-# # Output the RMSD matrix
-# print("RMSD Matrix:")
-# print(rmsd_matrix)
-# 
-
-# np.savetxt('clevin_rmsd_matrix.txt', rmsd_matrix, fmt='%d')
-
-# In[ ]:
-
-
-sns.reset_defaults()
-
-
-# In[ ]:
-
 
 vmin = 0.0
 vmax = 1.0
@@ -229,8 +155,6 @@ print(ids)
 plt.savefig('plot.pdf', dpi=600)
 plt.show()
 
-
-# In[71]:
 
 
 from scipy.cluster.vq import vq
@@ -262,14 +186,8 @@ print(f"Cluster centers:\n{kmeans.cluster_centers_}")
 
 
 
-# In[72]:
-
 
 print (kl.elbow)
-
-
-# In[76]:
-
 
 # 6. Use UMAP to reduce dimensionality for visualization
 umap_embedding = UMAP(n_components=2, random_state=42).fit_transform(scaled_features)
@@ -296,15 +214,11 @@ plt.savefig('/Users/mantingmu/Desktop/umap.pdf', dpi=300)
 plt.show()
 
 
-# In[78]:
-
 
 # 10. closest conformer files
 for c in closest:
     print (c+1)
 
-
-# In[83]:
 
 
 data = pd.DataFrame({
@@ -320,8 +234,6 @@ sorted_data = data.sort_values(by='label')
 for idx, x, y, label in zip(sorted_data['index'], sorted_data['x'], sorted_data['y'], sorted_data['label']):
     print(f"Index: {idx}, K-means label: {label}")
 
-
-# In[ ]:
 
 
 
